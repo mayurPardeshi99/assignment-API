13b8d615ee0433e7087957a1bd6fcd4ab33240a8
Mayur Pardeshi <81177732+mayurPardeshi99@users.noreply.github.com>
2021-12-21 07:55:07 +0530

    Add Comments

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,9 +1,11 @@
+// Page not found handler
 const notFoundHandler = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
+// Error handler
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
